61ec1173bd80386fd061319ab86c32171dc2eabe
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-01 23:18:47 +0300

    Created Domain

---
+++ Domain/Entities/MessageEntity.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Base;
+using System.ComponentModel;
+
+namespace Domain.Entities
+{
+    public class MessageEntity : BaseEntity
+    {
+        /// <summary>
+        /// Message content
+        /// </summary>
+        [Description("Message content")]
+        public required string Content { get; set; }
+
+        /// <summary>
+        /// Time of writing to the database
+        /// </summary>
+        [Description("Time of writing to the database")]
+        public DateTime SavedAt { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Time of sending by the user
+        /// </summary>
+        [Description("Time of sending by the user")]
+        public required DateTimeOffset SentAt { get; set; } 
+    }
+}
