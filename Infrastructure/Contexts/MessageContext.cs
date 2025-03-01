5deed84b7fb5d9eb634b3509dd80b9cd7495437d
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 00:40:19 +0300

    Created Infrastructure

---
+++ Infrastructure/Contexts/MessageContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Infrastructure.Contexts
+{
+    public class MessageContext
+    {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public MessageContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetSection("ConnectionStrings:PG").Value;
+        }
+    }
+}
