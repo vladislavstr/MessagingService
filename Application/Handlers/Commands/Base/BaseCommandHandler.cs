b6661192c999dd4ef5d9c58c70b0ec88fdaece39
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 00:46:11 +0300

    Created Application

---
+++ Application/Handlers/Commands/Base/BaseCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models.Handlers.Commands.Base;
+using MediatR;
+
+namespace Application.Handlers.Commands.Base
+{
+    public abstract class BaseCommandHandler<T> : IRequestHandler<T> where T : BaseCommand
+    {
+        public abstract Task Handle(T request, CancellationToken cancellationToken = default);
+    }
+
+    public abstract class BaseCommandHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : BaseCommand<TResponse>
+    {
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken = default);
+    }
+}
