a0bf6f61dadb1b89c2a6f593d709878402073e4a
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-03 00:56:22 +0300

    Initial created of controller

---
+++ Api/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using Api.Controllers.Base;
+using Domain.Models.Handlers.Commands.Message;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController(ISender sender) : BaseController<MessageController>(sender)
+    {
+        /// <summary>
+        /// Create of message - add to data base
+        /// </summary>
+        /// <param name="createMessageCommand"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create-message")]
+        public async Task<IActionResult> RemovePatient
+            (
+                CreateMessageCommand createMessageCommand,
+                CancellationToken cancellationToken = default
+            ) => await Action(createMessageCommand, cancellationToken);
+    }
+}
