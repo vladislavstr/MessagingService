cb5ac78aef167a26eda4418f3b0c1f8995a3a603
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 23:50:23 +0300

    Created MessageProvider

---
+++ Application/Providers/Interfaces/IMessageProvider.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Application.Providers.Interfaces
+{
+    public interface IMessageProvider
+    {
+        int AddMessage(MessageModel patient, bool isContinuation = false);
+    }
+}
