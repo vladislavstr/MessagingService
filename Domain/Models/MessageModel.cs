3a0c7ead1e5c2fc9344b78dd77a77c3f5c86ca77
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 23:49:40 +0300

    Created CreateMessageCommandValidator

---
+++ Domain/Models/MessageModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Models
+{
+    public class MessageModel
+    {
+        /// <summary>
+        /// Message id
+        /// </summary>
+        [Description("Message id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Message content
+        /// </summary>
+        [Description("Message content")]
+        public required string Content { get; set; }
+
+        /// <summary>
+        /// Time of writing to the database
+        /// </summary>
+        [Description("Time of writing to the database")]
+        public DateTime SavedAt { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Time of sending by the user
+        /// </summary>
+        [Description("Time of sending by the user")]
+        public required DateTimeOffset SentAt { get; set; }
+    }
+}
