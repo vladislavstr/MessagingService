35532415e37a7b926ea52c22be3380898eca1c39
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-03 00:54:58 +0300

    Created onfigures

---
+++ Api/Configures/InfrastructureConfigure.cs
@@ -0,0 +1,15 @@
+﻿using Application.Providers;
+using Application.Providers.Interfaces;
+
+namespace Api.Configures
+{
+    public static class InfrastructureConfigure
+    {
+        public static IServiceCollection AddInfrastructureConfigure(this IServiceCollection services)
+        {
+            services.AddSingleton<IMessageProvider, MessageProvider>();
+
+            return services;
+        }
+    }
+}
---
+++ Api/Configures/ValidatorsConfigure.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Handlers.Commands.Message;
+using Domain.Models.Handlers.Commands.Message.Validators;
+using FluentValidation;
+
+namespace Api.Configures
+{
+    public static class ValidatorsConfigure
+    {
+        public static IServiceCollection AddValidatorsConfigure(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<CreateMessageCommand>, CreateMessageCommandValidator>();
+
+            return services;
+        }
+    }
+}
