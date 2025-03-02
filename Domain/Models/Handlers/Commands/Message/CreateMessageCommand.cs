4108906876de883b931e96c824cd78e79b384bae
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 23:48:19 +0300

    Created CreateMessageCommand

---
+++ Domain/Models/Handlers/Commands/Message/CreateMessageCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.Handlers.Commands.Base;
+
+namespace Domain.Models.Handlers.Commands.Message
+{
+    public sealed record CreateMessageCommand
+    (
+        int Id,
+        string Content,
+        DateTime SavedAt,
+        DateTimeOffset SentAt
+    ) : BaseCommand<string>;
+}
