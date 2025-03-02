1200acc84aaf6c09dc376944d0c168aa3d5e60d9
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-02 23:51:00 +0300

    Created MessageMapper

---
+++ Api/Configures/MapperConfigure.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Mappers.Message;
+
+namespace Api.Configures
+{
+    public static class MapperConfigure
+    {
+        public static IServiceCollection AddMapperConfigure(this IServiceCollection services)
+        {
+            services.AddSingleton<IMessageMapper, MessageMapper>();
+
+            return services;
+        }
+    }
+}
---
+++ Domain/Mappers/Message/MessageMapper.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Domain.Models;
+using Domain.Models.Handlers.Commands.Message;
+using Riok.Mapperly.Abstractions;
+
+namespace Domain.Mappers.Message
+{
+    public interface IMessageMapper
+    {
+        MessageModel ToModel(CreateMessageCommand command);
+        MessageModel ToModel(MessageEntity entity);
+        MessageEntity ToEntity(MessageModel model);
+    }
+
+    [Mapper]
+    public partial class MessageMapper : IMessageMapper
+    {
+        [MapProperty(nameof(CreateMessageCommand.Id), nameof(MessageModel.Id))]
+        public partial MessageModel ToModel(CreateMessageCommand command);
+
+        [MapProperty(nameof(MessageEntity.Id), nameof(MessageModel.Id))]
+        public partial MessageModel ToModel(MessageEntity entity);
+
+        [MapperIgnoreTarget(nameof(MessageEntity.Id))]
+        [MapProperty(nameof(MessageModel.Id), nameof(MessageEntity.Id))]
+        public partial MessageEntity ToEntity(MessageModel model);
+    }
+}
