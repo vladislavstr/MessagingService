067632dc89652bf6e8b50968b1e69daf6efea78b
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-03 22:51:27 +0300

    Created InitializeDatabase

--- Infrastructure/Contexts/MessageContext.cs
+++ Infrastructure/Contexts/MessageContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Domain.Entities;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 
 namespace Infrastructure.Contexts
@@ -11,6 +12,22 @@ namespace Infrastructure.Contexts
         public MessageContext(IConfiguration configuration)
         {
             _connectionString = configuration.GetSection("ConnectionStrings:PG").Value;
+        }
+
+        public static void InitializeDatabase(string connectionString)
+        {
+            using var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            var command = new NpgsqlCommand(@"
+                                                CREATE TABLE IF NOT EXISTS messages (
+                                                Id SERIAL PRIMARY KEY,
+                                                Content VARCHAR(128) NOT NULL,
+                                                SavedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                SentAt TIMESTAMP WITH TIME ZONE NOT NULL);
+                                             ", connection);
+
+            command.ExecuteNonQuery();
         }
     }
 }
