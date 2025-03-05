39167e0835e08dfcd1af2bb420b920d29c38de51
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-06 01:46:04 +0300

    Added event

---
+++ Application/Events/MessageCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Dtos;
+using MediatR;
+
+namespace Application.Events
+{
+    public class MessageCreatedEvent : INotification
+    {
+        public MessageDto Message { get; }
+
+        public MessageCreatedEvent(MessageDto message)
+        {
+            Message = message;
+        }
+    }
+}
