8e249010e7f430ded4a5a3cb06bf6232c7956e5c
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-06 23:43:58 +0300

    Initial create of Queries

---
+++ Application/Handlers/Queries/Base/BaseQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.Handlers.Queries.Base;
+using MediatR;
+
+namespace Application.Handlers.Queries.Base
+{
+    public abstract class BaseQueryHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : BaseQuery<TResponse>
+    {
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken = default);
+    }
+}
