e2c6cc605e5cd3a742a2b589454bcc54ed5fbea6
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-07 00:16:08 +0300

    Created models

---
+++ Domain/Models/Handlers/Queries/Message/GetMessageQuery.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Models.Handlers.Queries.Base;
+using Domain.Models.Responses.Models;
+
+namespace Domain.Models.Handlers.Queries.Message
+{
+    public sealed record GetMessageQuery : BaseQuery<MessagesResponse>;
+}
