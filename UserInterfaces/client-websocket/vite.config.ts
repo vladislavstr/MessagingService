1d76c9a2516d46338bbf89ce864c93369325b0e9
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-10 00:12:07 +0300

    Fix ports

--- UserInterfaces/client-websocket/vite.config.ts
+++ UserInterfaces/client-websocket/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 60719,
-    }
-})
+  plugins: [plugin()],
+  server: {
+    port: 60718,
+  },
+});
