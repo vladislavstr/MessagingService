8fb3b7312d3b7bf6fae2e5e36027e717bd6972f8
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-10 21:46:22 +0300

    Initial create of Aspire

---
+++ Core/AspireHost/Program.cs
@@ -0,0 +1,27 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+Console.WriteLine(builder.Environment.ToString());
+
+const string scheme = "http";
+
+var service = builder.AddProject<Projects.Api>("api").WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development");
+
+var sender = builder.AddNpmApp("client-sender", "../../UserInterfaces/client-sender")
+                .WithEndpoint(port: 60717, scheme: scheme, env: "VITE_PORT")
+                .WithReference(service)
+                .WaitFor(service)
+                .PublishAsDockerFile();
+
+builder.AddNpmApp("client-websocket", "../../UserInterfaces/client-websocket")
+    .WithEndpoint(port: 60718, scheme: scheme, env: "VITE_PORT")
+    .WithReference(service)
+    .WaitFor(sender)
+    .PublishAsDockerFile();
+
+builder.AddNpmApp("client-recent-messages", "../../UserInterfaces/client-recent-messages")
+    .WithEndpoint(port: 60719, scheme: scheme, env: "VITE_PORT")
+    .WithReference(service)
+    .WaitFor(sender)
+    .PublishAsDockerFile();
+
+builder.Build().Run();
