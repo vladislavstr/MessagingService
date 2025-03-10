7325dfb8fdd50422f657ab5b70582ce8f5b78939
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-10 21:51:20 +0300

    Fix

--- UserInterfaces/client-recent-messages/vite.config.ts
+++ UserInterfaces/client-recent-messages/vite.config.ts
@@ -1,37 +1,32 @@
-//import { defineConfig } from "vite";
-import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-//// https://vite.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//  server: {
-//    port: 60719,
-//  },
-//});
-
-
 export default defineConfig(({ command, mode }: ConfigEnv) => {
-    console.log(`configuring vite with command: ${command}, mode: ${mode}`);
-    // suppress eslint warning that process isn't defined (it is)
-    // eslint-disable-next-line
-    const cwd = process.cwd();
-    console.log(`loading envs from ${cwd} ...`);
-    const env = { ...loadEnv(mode, cwd, 'VITE_') };
-    console.log(`loaded env: ${JSON.stringify(env)}`);
+  console.log(`configuring vite with command: ${command}, mode: ${mode}`);
+  // suppress eslint warning that process isn't defined (it is)
+  // eslint-disable-next-line
+  const cwd = process.cwd();
+  console.log(`loading envs from ${cwd} ...`);
+  const env = { ...loadEnv(mode, cwd, "VITE_") };
+  console.log(`loaded env: ${JSON.stringify(env)}`);
 
-    // reusable config for both server and preview
-    const serverConfig = {
-        host: true,
-        port: Number(env.VITE_PORT),
-        strictPort: true,
-        https: false,
-    };
+  const port = Number(env.VITE_PORT);
+  if (isNaN(port)) {
+    throw new Error(`Invalid port number: ${env.VITE_PORT}`);
+  }
 
-    return {
-        base: '/',
-        plugins: [react()],// basicSsl()],
-        preview: serverConfig,
-        server: serverConfig,
-    };
+  // reusable config for both server and preview
+  const serverConfig = {
+    host: true,
+    port: port,
+    strictPort: true,
+    https: false,
+  };
+
+  return {
+    base: "/",
+    plugins: [react()], // basicSsl()],
+    preview: serverConfig,
+    server: serverConfig,
+  };
 });
--- UserInterfaces/client-websocket/vite.config.ts
+++ UserInterfaces/client-websocket/vite.config.ts
@@ -1,44 +1,32 @@
-//import { defineConfig } from "vite";
-import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// import plugin from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//  plugins: [plugin()],
-//  server: {
-//    port: 60718,
-//  },
-// });
-
 export default defineConfig(({ command, mode }: ConfigEnv) => {
-    console.log(`configuring vite with command: ${command}, mode: ${mode}`);
-    // suppress eslint warning that process isn't defined (it is)
-    // eslint-disable-next-line
-    const cwd = process.cwd();
-    console.log(`loading envs from ${cwd} ...`);
-    const env = { ...loadEnv(mode, cwd, 'VITE_') };
-    console.log(`loaded env: ${JSON.stringify(env)}`);
+  console.log(`configuring vite with command: ${command}, mode: ${mode}`);
+  // suppress eslint warning that process isn't defined (it is)
+  // eslint-disable-next-line
+  const cwd = process.cwd();
+  console.log(`loading envs from ${cwd} ...`);
+  const env = { ...loadEnv(mode, cwd, "VITE_") };
+  console.log(`loaded env: ${JSON.stringify(env)}`);
 
-    const port = Number(env.VITE_PORT);
-    if (isNaN(port)) {
-        
-        throw new Error(`Invalid port number: ${env.VITE_PORT}`);
-    }
+  const port = Number(env.VITE_PORT);
+  if (isNaN(port)) {
+    throw new Error(`Invalid port number: ${env.VITE_PORT}`);
+  }
 
-    // reusable config for both server and preview
-    const serverConfig = {
-        host: true,
-        port: port,
-        strictPort: true,
-        https: false,
-    };
+  // reusable config for both server and preview
+  const serverConfig = {
+    host: true,
+    port: port,
+    strictPort: true,
+    https: false,
+  };
 
-    return {
-        base: '/',
-        plugins: [react()],// basicSsl()],
-        preview: serverConfig,
-        server: serverConfig,
-    };
+  return {
+    base: "/",
+    plugins: [react()], // basicSsl()],
+    preview: serverConfig,
+    server: serverConfig,
+  };
 });
