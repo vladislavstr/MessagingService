f615ed39488bc2f0f54bee081536406605009f79
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-14 22:53:10 +0300

    Initial create tests of Application layer

---
+++ Tests/Application.Tests/GetMessagesQueryHandlerTest.cs
@@ -0,0 +1,71 @@
+﻿using Application.Handlers.Queries.Message;
+using Application.Providers.Interfaces;
+using Domain.Dtos;
+using Domain.Entities;
+using Domain.Mappers.Message;
+using Domain.Models.Handlers.Queries.Message;
+using Moq;
+
+namespace Application.Tests
+{
+    public class GetMessagesQueryHandlerTest
+    {
+        private Mock<IMessageMapper> _messageMapperMock;
+        private Mock<IDataBaseProvider> _dataBaseProviderMock;
+        private GetMessagesQueryHandler _handler;
+
+        public GetMessagesQueryHandlerTest()
+        {
+            _messageMapperMock = new Mock<IMessageMapper>();
+            _dataBaseProviderMock = new Mock<IDataBaseProvider>();
+            _handler = new GetMessagesQueryHandler(_messageMapperMock.Object, _dataBaseProviderMock.Object);
+
+        }
+
+        [Fact]
+        public async Task Handle_ValidRequest_ReturnsMessagesResponse()
+        {
+            // Arrange
+            var request = new GetMessageQuery();
+            var messages = new List<MessageEntity>
+            {
+                new MessageEntity { Id = 1, Content = "Message 1", SavedAt = DateTime.UtcNow },
+                new MessageEntity { Id = 2, Content = "Message 2", SavedAt = DateTime.UtcNow }
+            };
+            var messageDtos = new List<MessageDto>
+            {
+                new MessageDto { Id = messages[0].Id, Content = messages[0].Content, SavedAt = messages[0].SavedAt },
+                new MessageDto { Id = messages[1].Id, Content = messages[1].Content, SavedAt = messages[1].SavedAt }
+            };
+
+            _dataBaseProviderMock
+                .Setup(db => db.GetMessagesAsync())
+                .ReturnsAsync(messages);
+
+            _messageMapperMock
+                .Setup(mapper => mapper.ToDto(It.IsAny<IEnumerable<MessageEntity>>()))
+                .Returns(messageDtos);
+
+            // Act
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(messageDtos, result.Messages);
+        }
+
+        [Fact]
+        public async Task Handle_DatabaseThrowsException_ThrowsException()
+        {
+            // Arrange
+            var request = new GetMessageQuery();
+
+            _dataBaseProviderMock
+                .Setup(db => db.GetMessagesAsync())
+                .ThrowsAsync(new Exception("Database error"));
+
+            // Act & Assert
+            await Assert.ThrowsAsync<Exception>(() => _handler.Handle(request, CancellationToken.None));
+        }
+    }
+}
