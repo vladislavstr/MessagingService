0cc3333bc74267a86ce931f932916fb5d39aaf5a
Владислав Стрельников <vladmuskat@yandex.ru>
2025-03-17 19:28:13 +0300

    Added postgres and update seq to container

--- Core/AspireHost/Program.cs
+++ Core/AspireHost/Program.cs
@@ -4,11 +4,27 @@ Console.WriteLine(builder.Environment.ToString());
 
 const string scheme = "http";
 
-var seq = builder.AddSeq("seq", 5341);
+var seq = builder.AddContainer("seq", "datalust/seq")
+    .WithEnvironment("ACCEPT_EULA", "Y")
+    .WithVolume("seq-data", "/data")
+        .WithEndpoint(
+        port: 5341,
+        targetPort: 80,
+        name: "seq-endpoint",
+        scheme: "http"
+    );
+
+var postgres = builder.AddContainer("postgres", "postgres:17")
+    .WithEnvironment("POSTGRES_USER", "TestUser")
+    .WithEnvironment("POSTGRES_PASSWORD", "1234")
+    .WithEnvironment("POSTGRES_DB", "TestDb")
+    .WithVolume("postgres-data", "/var/lib/postgresql/data") 
+    .WithEndpoint(port: 5432, targetPort: 5432, name: "postgres-endpoint") 
+    .WaitFor(seq); 
 
 var service = builder.AddProject<Projects.Api>("api")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WaitFor(seq);
+    .WaitFor(postgres);
 
 var sender = builder.AddNpmApp("client-sender", "../../UserInterfaces/client-sender")
     .WithEndpoint(port: 60717, scheme: scheme, env: "VITE_PORT")
